7b51994287b2c3adac6b2f5a2ccd68c3f94ed714
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-11-17 09:47:24 +0600

    Final Modal Window

--- script.js
+++ script.js
@@ -22,3 +22,9 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener("click", CloseModal);
 overlay.addEventListener("click", CloseModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    CloseModal();
+  }
+});
